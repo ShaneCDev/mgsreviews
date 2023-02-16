27b6b531e9c6b847b9b99b70b54b88ca5f071eec
Shane <shaneconway1998@gmail.com>
2023-02-16 17:02:14 +0000

    added models and registered them in admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from . models import MGS, Review
 
-# Register your models here.
+admin.site.register(MGS)
+admin.site.register(Review)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MGS(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=3000)
+    poster = models.FileField(upload_to='')
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='movie_reviews')
+    review_date = models.DateTimeField(default=timezone.now)
+    scores = (
+        (1, 1),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5)
+    )
+    stars = models.IntegerField(choices=scores)
+    comment = models.TextField(max_length=4000)
+    movie = models.ForeignKey(MGS, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.MGS.title
