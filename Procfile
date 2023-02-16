415c651f3678cd85f5d546be7ee972d0f0f3ec3c
Shane <shaneconway1998@gmail.com>
2023-02-16 15:58:18 +0000

    renamed files to fix deployment issue

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn reddit.wsgi
+web: gunicorn review.wsgi
--- review/settings.py
+++ review/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'blog',
+    'movies',
 ]
 
 MIDDLEWARE = [
