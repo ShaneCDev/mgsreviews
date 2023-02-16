faeec2f23bc200fb37a1d4ec93241b7b9888418d
Shane <shaneconway1998@gmail.com>
2023-02-16 16:05:20 +0000

    fixing deployment version of app

--- review/wsgi.py
+++ review/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'review.settings')
 
 application = get_wsgi_application()
