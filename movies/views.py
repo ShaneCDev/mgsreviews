0a0c4b844cb45392f57f0b6ce01dedc239d70463
Shane <shaneconway1998@gmail.com>
2023-02-20 16:37:17 +0000

    added a home page with a carousel that I have to fix as its bugged, and started on other pages that also need to be fixed as I am getting templaterender errors

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Movie, Game, Show
 
 
 def home(request):
-    return render(request, 'base.html', {})
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies
+    }
+    return render(request, 'index.html', context)
+
+
+def movies(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies
+    }
+    return render(request, 'movies.html', context)
