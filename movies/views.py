97c5fcb48800378f48706b47eb52818b93fbfa42
Shane <shaneconway1998@gmail.com>
2023-02-22 18:43:20 +0000

    added shows.html and view for it

--- movies/views.py
+++ movies/views.py
@@ -31,9 +31,33 @@ def games(request):
     return render(request, 'games.html', context)
 
 
+def shows(request):
+    shows = Show.objects.all()
+    context = {
+        'shows': shows
+    }
+    return render(request, 'shows.html', context)
+
+
 def movie_detail(request, id):
     movie = Movie.objects.get(id=id)
     context = {
         'movie': movie
     }
     return render(request, 'movie_detail.html', context)
+
+
+def game_detail(request, id):
+    game = Game.objects.get(id=id)
+    context = {
+        'game': game
+    }
+    return render(request, 'game_detail.html', context)
+
+
+def show_detail(request, id):
+    show = Show.objects.get(id=id)
+    context = {
+        'show': show
+    }
+    return render(request, 'show_detail.html', context)
