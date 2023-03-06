9f75fa53324b504d4635194829d48f9fb4564000
Shane <shaneconway1998@gmail.com>
2023-03-06 20:23:45 +0000

    updated the other carousels on the home page

--- movies/tests.py
+++ movies/tests.py
@@ -18,3 +18,19 @@ class TestViews(TestCase):
         response = self.client.get('/games')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'games.html')
+
+    def test_shows_page(self):
+        response = self.client.get('/shows')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'shows.html')
+
+
+class TestModels(TestCase):
+
+    def create_movie(self, title='test', description='this is a test', director='testdirector', cast='testcast', writer='testwriter', genre='testgenre'):
+        return Movie.objects.create(title=title, description=description, director=director, cast=cast, writer=writer, genre=genre)
+
+    def create_movie_test(self):
+        movie = self.create_movie()
+        self.assertTrue(isinstance(movie, Movie))
+        self.assertEqual(movie.__str__, movie.title)
