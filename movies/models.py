92b363acb3b7421d9f68becd2e596d1fe38a2350
Shane <shaneconway1998@gmail.com>
2023-03-07 13:57:55 +0000

    updated the games model and games page

--- movies/models.py
+++ movies/models.py
@@ -27,6 +27,7 @@ class Game(models.Model):
     cast = models.CharField(max_length=100, null=True)
     writer = models.CharField(max_length=50, null=True)
     genre = models.CharField(max_length=100, null=True)
+    synopsis = models.CharField(max_length=500, null=True)
 
     def __str__(self):
         return self.title
