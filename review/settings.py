88c9286925fc4b159de3f0c36f69aa6811175d76
Shane <36411961+ShaneCDev@users.noreply.github.com>
2023-04-25 11:24:30 +0100

    Changed hostname to reflect changes to the project name

--- review/settings.py
+++ review/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['reddjango.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mgsreviews.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
